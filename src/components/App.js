9dbd13ed4efce33be992efb26fbe8ae790211b86
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-04-30 12:10:25 +0700

    init component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NavComponent from './nav/nav-component';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <NavComponent/>
         </a>
       </header>
     </div>
---
+++ src/components/nav/nav-component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Nav Component</h1>
+            </div>
+        );
+    }
+}
+
+export default NavComponent;
